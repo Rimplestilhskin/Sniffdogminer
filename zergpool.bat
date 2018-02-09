b47e7f3e6cc04fca4c5979b0f740867f9f3ed541
pinpins <zergpools@gmail.com>
2018-02-09 16:15:55 +0200

    checkin

--- zergpool.bat
+++ zergpool.bat
@@ -1,3 +1,3 @@
-powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\SniffDog.ps1 -Username Tyredas -Workername Beeboop -RigName Sniffdog001 -Currency USD -Passwordcurrency BTC -interval 120 -Delay 1 -Wallet 1AUGPnjP9Kxo5GkjEGbGAXmUwmUEFDYRwP -Location US -PoolName zergpool -Type nvidia,cpu -Algorithm xevan,tribus,c11,skein,equihash,groestl,sib,Nist5,Lyra2RE2,neoscrypt,skunk,x17,lyra2z,yescrypt,blake2s 
+powershell -version 5.0 -noexit -executionpolicy bypass -windowstyle maximized -command "&.\SniffDog.ps1 -Username Tyredas -Workername Beeboop -RigName Sniffdog001 -Currency USD -Passwordcurrency BTC -interval 120 -Delay 1 -Wallet 1BhezbVfupFBSfRixXDhkr4npHWb7NDLkA -Location US -PoolName zergpool -Type nvidia,cpu -Algorithm xevan,tribus,c11,skein,equihash,groestl,sib,Nist5,Lyra2RE2,neoscrypt,skunk,x17,lyra2z,yescrypt,blake2s 
 
 
